00ea0b8ee3e28607c4c5340df0bf597fb59ad37a
BrettS89 <blsodie@gmail.com>
2019-06-10 12:30:35 -0400

    adding to error handling

--- src/utilities/errorHandling.js
+++ src/utilities/errorHandling.js
@@ -1,3 +1,4 @@
-exports.handleError = (req, err, endpoint) => {
+exports.handleError = (res, err, endpoint) => {
+  console.log(endpoint + err);
   res.status(500).json({ message: 'an error occured' });
 }
