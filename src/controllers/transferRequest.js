de0d374fd49991617e831eda3c510190b4532a48
BrettS89 <blsodie@gmail.com>
2019-06-15 17:08:18 -0400

    fixing params

--- src/controllers/transferRequest.js
+++ src/controllers/transferRequest.js
@@ -82,7 +82,7 @@ exports.employeeClaimTR = async (req, res) => {
 exports.cancel = async (req, res) => {
   try {
     const employeeId = await employeeAuth(req.header('authorization'));
-    await transferRequest.cancelTransferRequest(req.params.id);
+    await transferRequest.cancelTransferRequest(req.body.id);
     const openTransferRequests = await transferRequest.getOpenTransferRequests();
     const myTransferRequests =
       await transferRequest.getMyTransferRequests(employeeId);
