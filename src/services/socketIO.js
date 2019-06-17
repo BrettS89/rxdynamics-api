8e33b5de18c8a601276ea980b27ccea462591071
BrettS89 <blsodie@gmail.com>
2019-06-16 20:10:33 -0400

    fixing disconnect

--- src/services/socketIO.js
+++ src/services/socketIO.js
@@ -3,10 +3,10 @@ const io = require('../../index');
 exports.listenForConnection = () => {
   io.io.on('connection', (socket) => {
     console.log('made socket connection');
-  });
 
-  io.io.on('disconnectt', (socket) => {
-    socket.disconnect();
-    console.log('a socket was disconnected');
+    io.io.on('disconnect', (socket) => {
+      socket.disconnect();
+      console.log('a socket was disconnected');
+    });
   });
 };
