b890c3f0288c0e936e9204374189208893f875b9
BrettS89 <blsodie@gmail.com>
2019-06-21 15:17:31 -0400

    updating pharmacy model

--- src/utilities/errorHandling.js
+++ src/utilities/errorHandling.js
@@ -1,6 +1,6 @@
 exports.handleError = (res, err, endpoint) => {
   const message = err.message.message ? err.message.message : err.message;
-  console.log(endpoint + message);
   const status = err.message.status ? err.message.status : 500;
+  console.log(endpoint, message);
   res.status(status).json({ error: message });
 };
