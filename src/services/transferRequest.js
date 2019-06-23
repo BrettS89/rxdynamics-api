a51a74bbb5b80b5b3638b193dcf7090ced9456e4
BrettS89 <blsodie@gmail.com>
2019-06-23 08:53:17 -0400

    fixing calculation expire

--- src/services/transferRequest.js
+++ src/services/transferRequest.js
@@ -87,7 +87,7 @@ exports.initiate = async memberPhoneNumber => {
     throw { message: 'Could not find any Rx\'s', status: 404 };
   }
 
-  if (transferRequests.length === 1 && Date.now() - transferRequests[0].dateCreated > keys.expire)
+  if (transferRequests.length === 1 && Date.now() - transferRequests[0].dateCreated < keys.expire)
     return false;
 
   transferRequests = transferRequests.filter(t => Date.now() - t.dateCreated < keys.expire);
