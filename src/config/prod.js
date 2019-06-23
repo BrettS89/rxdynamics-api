9c3523eeb76d09206f82bb310d8e97d77ca8fab3
BrettS89 <blsodie@gmail.com>
2019-06-23 09:10:26 -0400

    testing expire

--- src/config/prod.js
+++ src/config/prod.js
@@ -5,5 +5,5 @@ module.exports = {
   twilioAccountSid: process.env.TWILIO_ACCOUNT_SID,
   twilioAuthToken: process.env.TWILIO_AUTH_TOKEN,
   twilioPhoneNumber: process.env.TWILIO_PHONE_NUMBER,
-  expire: 36000,
+  expire: 36,
 };
